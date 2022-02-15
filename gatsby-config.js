015bf29e7be64f87dc5a9515325429287f991671
Cynthia Henaff <cynthia@henaff.io>
2022-02-15 18:16:37 +0100

    feat: add env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-plausible`,
       options: {
-        domain: `paperjam.vercel.app`,
+        domain: process.env.GATSBY_PLAUSIBLE_DOMAIN,
       },
     },
   ],
